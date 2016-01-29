ec657c2d2bc685af2cbe5d94ec262b45130c89e1
S Anand <root.node@gmail.com>
2016-01-29 09:50:14 +0530

    Add test datasets. Try to upload into MySQL & PostgreSQL
    Squashed commit of the following:
    
    commit 693972ce9845d55c0048a50eca83da51a7b19dd9
    Author: S Anand <root.node@gmail.com>
    Date:   Fri Jan 29 09:48:16 2016 +0530
    
        If uploading into database fails, ignore and continue
    
    commit 91a382bf028b4b40ea6683c20bd1ab16d4cacf40
    Author: S Anand <root.node@gmail.com>
    Date:   Fri Jan 29 09:22:41 2016 +0530
    
        Skip tests if cannot connect to database
    
    commit b3874e48541a0dd99060bc0e96dd07cbd9f1221e
    Author: S Anand <root.node@gmail.com>
    Date:   Thu Jan 28 17:35:52 2016 +0530
    
        Replace dataset to work on PostgreSQL
    
    commit 1f8a8e4c64f3b16312c05bfbb06e73c4b7bac6c4
    Author: S Anand <root.node@gmail.com>
    Date:   Thu Jan 28 17:00:35 2016 +0530
    
        Remove tox duplication
    
    commit 1d8740e80f6bc0e14994037fb962098e05190268
    Author: S Anand <root.node@gmail.com>
    Date:   Thu Jan 28 16:46:15 2016 +0530
    
        WIP: test datasets

--- tests/test_autolysis.py
+++ tests/test_autolysis.py
@@ -9,30 +9,62 @@ Tests for `autolysis` module.
 """
 
 import os
+import sys
 import yaml
 import logging
 import unittest
 import autolysis
+import traceback
+import sqlalchemy as sa
+from odo import odo
 from blaze import Data
 from six.moves.urllib.request import urlretrieve
+from sqlalchemy_utils.functions import database_exists, create_database, drop_database
 
 _DIR = os.path.split(os.path.abspath(__file__))[0]
 _DATA_DIR = os.path.join(_DIR, 'data')
 
-# datasets.yaml documents sources & test results on datasets to be tested
-with open(os.path.join(_DIR, 'datasets.yaml')) as _dataset_file:
-    datasets = yaml.load(_dataset_file)
+# test_config.yaml documents sources & test results on datasets to be tested
+with open(os.path.join(_DIR, 'test_config.yaml')) as _dataset_file:
+    config = yaml.load(_dataset_file)
 
 
 def setUpModule():
     'Download test data files into data/ target folder'
+
+    # Download datasets
     if not os.path.exists(_DATA_DIR):
         os.makedirs(_DATA_DIR)
-    for dataset in datasets:
-        path = os.path.join(_DATA_DIR, dataset['path'])
-        if not os.path.exists(path):
-            logging.info('Downloading %s', dataset['path'])
-            urlretrieve(dataset['url'], path)
+    for dataset in config['datasets']:
+        dataset['path'] = os.path.join(_DATA_DIR, dataset['table'] + '.csv')
+        if not os.path.exists(dataset['path']):
+            logging.info('Downloading %s', dataset['table'])
+            urlretrieve(dataset['url'], dataset['path'])
+
+    # Create autolysis databases
+    # _databases('drop')
+    dburl = _databases('create')
+
+    # Load datasets into databases
+    for dataset in config['datasets']:
+        for db in dataset.get('databases', []):
+            if db not in dburl:
+                logging.warning('%s cannot use unconfigured DB %s', dataset['table'], db)
+                continue
+            db_url = dburl[db]
+
+            # Don't load data if a non-empty table already exists
+            target = dburl[db] + '::' + dataset['table']
+            engine = sa.create_engine(db_url)
+            if engine.dialect.has_table(engine.connect(), dataset['table']):
+                if Data(target).count() > 0:
+                    continue
+            logging.info('Creating table %s on %s', dataset['table'], db)
+            try:
+                odo(dataset['path'], target)
+            except sa.exc.InternalError as e:
+                logging.warning('Loading %s into %s failed: %s',
+                             dataset['table'], db, traceback.format_exc(0))
 
 
 class TestTypes(unittest.TestCase):
@@ -48,10 +80,39 @@ class TestTypes(unittest.TestCase):
                              set(expected[key]), '%s - %s' % (msg, key))
 
     def test_types(self):
-        for dataset in datasets:
+        for dataset in config['datasets']:
             data = Data(os.path.join(_DATA_DIR, dataset['path']))
             result = autolysis.types(data)
             self.check_type(result, dataset['types'], dataset['path'])
+
+
+def _databases(operation):
+    '''
+    _databases('create') creates all databases.
+    _databases('drop') drops all databases.
+    '''
+    # Drop autolysis databases. (sqlite3 data directory is _DATA_DIR)
+    os.chdir(_DATA_DIR)
+    result = {}
+    for db, db_url in config['databases'].items():
+        try:
+            engine = sa.create_engine(db_url)
+            engine.connect()
+        except sa.exc.OperationalError:
+            logging.warning('Unable to connect to %s to %s', db_url, operation)
+            continue
+        if 'create' in operation.lower():
+            if not database_exists(db_url):
+                logging.warning('Creating database %s', db_url)
+                create_database(db_url)
+            result[db] = db_url
+        if 'drop' in operation.lower():
+            if database_exists(db_url):
+                logging.warning('Dropping database %s', db_url)
+                drop_database(db_url)
+            result[db] = db_url
+    return result
+
 
 if __name__ == '__main__':
     import sys
