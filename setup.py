9c069193c707a7a9587c5dbeea9f30292c29f77a
S Anand <root.node@gmail.com>
2016-02-10 07:41:40 +0530

    Include release.json: a dependency for the package

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ setup(
     packages=find_packages(),
     package_dir={'autolysis':
                  'autolysis'},
+    # Read: http://stackoverflow.com/a/2969087/100904
+    # package_data includes data files for binary & source distributions
+    # include_package_data is only for source distributions, uses MANIFEST.in
+    package_data={
+      'autolysis': ['release.json']
+    },
     include_package_data=True,
     install_requires=[str(entry.req) for entry in
                       parse_requirements('requirements.txt', session=PipSession())],
